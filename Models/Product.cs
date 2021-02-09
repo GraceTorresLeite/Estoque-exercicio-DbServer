b7a3aad8be385db1686c04da8c0965de8fa04017
Grace Tôrres Leite <grace.torres@dbserver.com.br>
2021-02-09 12:48:36 -0300

    add variable razor

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace RazorPagesProduct.Models
     public class Product
     {
         public int ID { get; set; }
+
+        //site://khalidabuhakmeh.com/simplify-displayname-calls-in-razor
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
+
+        [Display(Name = "Quantidade")]
         public int Amount { get; set; }
     }
 }
